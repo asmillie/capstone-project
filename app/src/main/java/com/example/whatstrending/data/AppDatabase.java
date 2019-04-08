25bdaf8b95ef66528b30926548f059bf5051f293
Alan Smillie <alan.smillie@gmail.com>
2019-04-08 13:49:57 -0400

    Import AppDatabase and modify for current app

---
+++ app/src/main/java/com/example/whatstrending/data/AppDatabase.java
@@ -0,0 +1,33 @@
+package com.example.whatstrending.data;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.arch.persistence.room.TypeConverters;
+import android.content.Context;
+
+/*
+ * Room Database implemented following guide
+ * @ https://developer.android.com/training/data-storage/room/
+ * and Lesson 9 of the Udacity nanodegree program
+ */
+@Database(entities = {Article.class, Source.class}, version = 1, exportSchema = false)
+public abstract class AppDatabase extends RoomDatabase {
+
+    private static final Object LOCK = new Object();
+    private static final String DATABASE_NAME = "whats_trending";
+    private static AppDatabase sInstance;
+
+    static AppDatabase getInstance(Context context) {
+        if (sInstance == null) {
+            synchronized (LOCK) {
+                sInstance = Room.databaseBuilder(context.getApplicationContext(),
+                        AppDatabase.class, AppDatabase.DATABASE_NAME)
+                        .build();
+            }
+        }
+        return sInstance;
+    }
+
+    public abstract ArticleDao articleDao();
+}
