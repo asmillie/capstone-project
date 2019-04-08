274912ca07f6733a3de686857427065399f8d3b8
Alan Smillie <alan.smillie@gmail.com>
2019-04-08 12:11:57 -0400

    Move Api Key to header and modify query

--- app/src/main/java/com/example/whatstrending/network/NewsApiService.java
+++ app/src/main/java/com/example/whatstrending/network/NewsApiService.java
@@ -7,10 +7,13 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface NewsApiService {
     //https://newsapi.org/v2/top-headlines?country=us&apiKey=aeea4d6f7df04fc5aef5026a9ad2ff92
-    @GET("top-headlines?country={countryCode}&apiKey=" + BuildConfig.NEWSAPI_API_KEY)
-    Call<NewsApiResponse> getTopHeadlines(@Path("countryCode") String countryCode);
+    @Headers("X-Api-Key: " + BuildConfig.NEWSAPI_API_KEY)
+    @GET("top-headlines" )
+    Call<NewsApiResponse> getTopHeadlines(@Query("country") String countryCode);
 }
