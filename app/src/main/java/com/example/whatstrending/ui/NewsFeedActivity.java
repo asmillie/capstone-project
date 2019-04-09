651c5befa259aa42a758510d1bb64ec5bffc8520
Alan Smillie <alan.smillie@gmail.com>
2019-04-09 13:03:35 -0400

    Add RecyclerView to Layout

--- app/src/main/java/com/example/whatstrending/ui/NewsFeedActivity.java
+++ app/src/main/java/com/example/whatstrending/ui/NewsFeedActivity.java
@@ -2,17 +2,24 @@ package com.example.whatstrending.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 import com.example.whatstrending.R;
 import com.example.whatstrending.data.AppRepository;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class NewsFeedActivity extends AppCompatActivity {
+
+    @BindView(R.id.news_feed_rv)
+    RecyclerView mNewsFeedRV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AppRepository.getInstance(this);
+        ButterKnife.bind(this);
     }
 }
