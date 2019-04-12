e4726e76b371a6d0bda99ba968874665a3cef3b1
Alan Smillie <alan.smillie@gmail.com>
2019-04-12 10:30:58 -0400

    Get/Refresh article

--- app/src/main/java/com/example/whatstrending/ui/ArticleViewModel.java
+++ app/src/main/java/com/example/whatstrending/ui/ArticleViewModel.java
@@ -1,16 +1,31 @@
 package com.example.whatstrending.ui;
 
+import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.ViewModel;
 
 import com.example.whatstrending.data.AppRepository;
+import com.example.whatstrending.data.Article;
 
 public class ArticleViewModel extends ViewModel {
 
     private final AppRepository mRepository;
     private final int mArticleId;
 
+    private LiveData<Article> mArticle;
+
     ArticleViewModel(AppRepository appRepository, int articleId) {
         this.mRepository = appRepository;
         this.mArticleId = articleId;
+    }
+
+    LiveData<Article> getArticle() {
+        if (mArticle == null) {
+            refreshArticle();
+        }
+        return mArticle;
+    }
+
+    private void refreshArticle() {
+        mArticle = mRepository.getArticleById(mArticleId);
     }
 }
