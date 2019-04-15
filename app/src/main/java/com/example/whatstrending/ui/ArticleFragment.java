d67e6402b326856afdc9c53abc78362cf01fb433
Alan Smillie <alan.smillie@gmail.com>
2019-04-15 12:51:50 -0400

    Collapsing parallax scroll and article fields

--- app/src/main/java/com/example/whatstrending/ui/ArticleFragment.java
+++ app/src/main/java/com/example/whatstrending/ui/ArticleFragment.java
@@ -1,15 +1,23 @@
 package com.example.whatstrending.ui;
 
 
+import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.whatstrending.R;
+import com.example.whatstrending.data.Article;
+import com.example.whatstrending.utils.DateUtils;
+import com.squareup.picasso.Picasso;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 
@@ -24,6 +32,23 @@ public class ArticleFragment extends Fragment {
 
     private int mArticleId;
     private Unbinder mUnbinder;
+
+    private Article mArticle;
+
+    @BindView(R.id.title)
+    TextView mTitle;
+
+    @BindView(R.id.date_published)
+    TextView mDatePublished;
+
+    @BindView(R.id.author)
+    TextView mAuthor;
+
+    @BindView(R.id.content)
+    TextView mContent;
+
+    @BindView(R.id.article_image)
+    ImageView mArticleImage;
 
     public ArticleFragment() {
         // Required empty public constructor
@@ -68,5 +93,34 @@ public class ArticleFragment extends Fragment {
         ArticleViewModelFactory factory = new ArticleViewModelFactory(getActivity().getApplication(), mArticleId);
 
         ArticleViewModel viewModel = ViewModelProviders.of(getActivity(), factory).get(ArticleViewModel.class);
+        viewModel.getArticle().observe(this, new Observer<Article>() {
+            @Override
+            public void onChanged(@Nullable Article article) {
+                if (article != null) {
+                    mArticle = article;
+                    populateUI();
+                }
+            }
+        });
+    }
+
+    private void populateUI() {
+        if (mArticle != null) {
+
+            String articleImage = mArticle.getUrlToImage();
+            if (articleImage != null && !articleImage.equals("")) {
+                //TODO: Sizing
+                Picasso.get().load(articleImage).into(mArticleImage);
+            }
+
+            mTitle.setText(mArticle.getTitle());
+
+            String datePublished = DateUtils.formatUTCDateString(mArticle.getPublishedAt());
+            mDatePublished.setText(datePublished);
+
+            mAuthor.setText(mArticle.getAuthor());
+
+            mContent.setText(mArticle.getContent());
+        }
     }
 }
