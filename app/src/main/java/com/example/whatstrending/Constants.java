9107d8459184fd5110ecbe13a105142919c58c93
Alan Smillie <alan.smillie@gmail.com>
2019-04-23 10:17:14 -0400

    Get RemoteViews for Headlines

--- app/src/main/java/com/example/whatstrending/Constants.java
+++ app/src/main/java/com/example/whatstrending/Constants.java
@@ -4,9 +4,18 @@ public class Constants {
 
     private Constants() {}
 
+    /** App Path **/
+    private static final String APP_BASE_URI = "com.example.whatstrending";
+
     public static final String EXTRA_ARTICLE_ID = "article-id";
 
     public static final int EXTRA_ARTICLE_ID_DEFAULT = -1;
 
+    /** Widget Constants **/
     public static final int WIDGET_HEADLINE_LIMIT = 25;
+
+    public static final String WIDGET_ACTION_PREFIX = APP_BASE_URI + ".WIDGET_ACTION.";
+    public static final String WIDGET_EXTRA_PREFIX = APP_BASE_URI + ".WIDGET_EXTRA.";
+
+    public static final String WIDGET_VIEW_ARTICLE_ACTION = WIDGET_ACTION_PREFIX + "VIEW_ARTICLE";
 }
--- app/src/main/java/com/example/whatstrending/widget/LatestHeadlinesAppWidget.java
+++ app/src/main/java/com/example/whatstrending/widget/LatestHeadlinesAppWidget.java
@@ -1,11 +1,15 @@
 package com.example.whatstrending.widget;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
 
+import com.example.whatstrending.Constants;
 import com.example.whatstrending.R;
+import com.example.whatstrending.ui.ArticleActivity;
 
 /**
  * Implementation of App Widget functionality.
@@ -15,10 +19,7 @@ public class LatestHeadlinesAppWidget extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
-        // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.latest_headlines_app_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+        RemoteViews views = getHeadlines(context, appWidgetId);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
@@ -40,6 +41,27 @@ public class LatestHeadlinesAppWidget extends AppWidgetProvider {
     @Override
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
+    }
+
+    private static RemoteViews getHeadlines(Context context, int appWidgetId) {
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.latest_headlines_app_widget);
+
+        //Set List Adapter for Headlines
+        Intent intent = new Intent(context, HeadlinesWidgetService.class);
+        views.setRemoteAdapter(R.id.latest_headlines, intent);
+        views.setEmptyView(R.id.latest_headlines, R.id.empty_view);
+        //Set Intent with View Article action
+        Intent viewArticleIntent = new Intent(context, ArticleActivity.class);
+        viewArticleIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+        viewArticleIntent.setAction(Constants.WIDGET_VIEW_ARTICLE_ACTION);
+        //Set Pending Intent for each headline list item, assigning the view article intent to each
+        PendingIntent selectArticlePendingIntent = PendingIntent.getBroadcast(context,
+                0,
+                viewArticleIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        views.setPendingIntentTemplate(R.id.latest_headlines, selectArticlePendingIntent);
+
+        return views;
     }
 }
 
