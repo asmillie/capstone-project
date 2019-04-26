5680261df769dbec7e0c280c263698b7a4f83a9c
Alan Smillie <alan.smillie@gmail.com>
2019-04-26 13:48:40 -0400

    Add list adapter and bind views

--- app/src/main/java/com/example/whatstrending/ui/SearchActivity.java
+++ app/src/main/java/com/example/whatstrending/ui/SearchActivity.java
@@ -5,8 +5,14 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import com.example.whatstrending.Constants;
 import com.example.whatstrending.R;
@@ -14,23 +20,43 @@ import com.example.whatstrending.data.Article;
 import com.example.whatstrending.data.NewsApiResponse;
 import com.example.whatstrending.network.NewsApiClient;
 import com.example.whatstrending.network.NewsApiService;
+import com.example.whatstrending.utils.AnalyticsUtils;
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 import java.io.IOException;
 import java.util.List;
 
+import butterknife.BindBool;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import retrofit2.Call;
 
-public class SearchActivity extends AppCompatActivity {
+public class SearchActivity extends AppCompatActivity implements ArticleListAdapter.ArticleClickListener {
 
     private static final String TAG = SearchActivity.class.getSimpleName();
 
+    private FirebaseAnalytics mFirebaseAnalytics;
+
     private List<Article> mArticleList;
+    private ArticleListAdapter mArticleListAdapter;
     private int totalResults;
+
+    @BindView(R.id.article_search_results)
+    RecyclerView mArticleSearchResults;
+
+    @BindView(R.id.loading_bar)
+    ProgressBar mLoadingBar;
+
+    @BindBool(R.bool.is_large_screen_device)
+    boolean mIsLargeScreenDevice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        ButterKnife.bind(this);
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
 
         totalResults = Constants.TOTAL_RESULTS_DEFAULT;
 
@@ -43,7 +69,51 @@ public class SearchActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onArticleClick(int articleId) {
+        AnalyticsUtils.logArticleSelect(mFirebaseAnalytics, articleId);
+
+        Intent intent = new Intent(SearchActivity.this, ArticleActivity.class);
+        intent.putExtra(Constants.EXTRA_ARTICLE_ID, articleId);
+        startActivity(intent);
+    }
+
+    private void initViews() {
+        mArticleListAdapter = new ArticleListAdapter(null, this);
+
+        RecyclerView.LayoutManager layoutManager;
+        if (!mIsLargeScreenDevice) {
+            layoutManager = new LinearLayoutManager(this,
+                    LinearLayoutManager.VERTICAL, false);
+        } else {
+            layoutManager = new GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false);
+        }
+
+        mArticleSearchResults.setLayoutManager(layoutManager);
+        mArticleSearchResults.setAdapter(mArticleListAdapter);
+
+        DividerItemDecoration itemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        mArticleSearchResults.addItemDecoration(itemDecoration);
+
+        mArticleListAdapter.setArticleList(mArticleList);
+    }
+
+    private void showLoading() {
+        mLoadingBar.setVisibility(View.VISIBLE);
+        mArticleSearchResults.setVisibility(View.GONE);
+    }
+
+    private void showSearchResults() {
+        mLoadingBar.setVisibility(View.GONE);
+        mArticleSearchResults.setVisibility(View.VISIBLE);
+    }
+
     private class SearchAsyncTask extends AsyncTask<String, Void, List<Article>> {
+
+        @Override
+        protected void onPreExecute() {
+            showLoading();
+        }
 
         @Override
         protected List<Article> doInBackground(String... query) {
@@ -76,7 +146,8 @@ public class SearchActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(List<Article> articles) {
             mArticleList = articles;
-
+            showSearchResults();
+            //TODO: Notify adapter
         }
     }
 }
